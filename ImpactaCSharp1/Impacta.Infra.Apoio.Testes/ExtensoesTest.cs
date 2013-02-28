778333db48153d2255ebac707ec852850f4cbdc5
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2013-02-28 10:30:00 +0000

    Teste para ConverterParaDateTime.

---
+++ ImpactaCSharp1/Impacta.Infra.Apoio.Testes/ExtensoesTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Impacta.Infra.Apoio.Testes
+{
+    [TestClass]
+    public class ExtensoesTest
+    {
+        [TestMethod]
+        public void ConverterParaDateTimeTest()
+        {
+            string possivelData = "00/00/0000";
+            DateTime esperado = DateTime.MinValue;
+            DateTime retornado;
+            retornado = possivelData.ConverterParaDateTime();
+            Assert.AreEqual(esperado, retornado);
+        }
+    }
+}
