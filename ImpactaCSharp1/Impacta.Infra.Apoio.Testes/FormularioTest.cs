3ae74e5b698f788278779a45f7d547535cd647a7
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2013-05-19 23:12:08 +0000

    Validação de nome sem número.

---
+++ ImpactaCSharp1/Impacta.Infra.Apoio.Testes/FormularioTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Forms;
+
+namespace Impacta.Infra.Apoio.Testes
+{
+    [TestClass]
+    public class FormularioTest
+    {
+        [TestMethod]
+        public void ValidarTipoDosDadosTest()
+        {
+            var formulario = new Form();
+            var provedorDeErro = new ErrorProvider();
+
+            formulario.Controls.Add(new TextBox { Text = "Vítor", Tag = "TEXTOSEMNUMEROS" });
+            var retorno = Formulario.ValidarTipoDosDados(formulario, provedorDeErro);
+            Assert.IsTrue(retorno);
+
+            formulario.Controls.Clear();
+            formulario.Controls.Add(new TextBox { Text = "V2ítor", Tag = "TEXTOSEMNUMEROS" });
+            retorno = Formulario.ValidarTipoDosDados(formulario, provedorDeErro);
+            Assert.IsFalse(retorno);
+        }
+    }
+}
