1fdf655a0f00225c43de4b0add5b8a2972bff340
vitoravelino_cp <SND\vitoravelino_cp>
2013-05-31 18:13:47 +0000

    Reestruturação das pastas e projetos.

---
+++ Alunos/Pizzaria/Pizzaria.Infra.Repositorios.Excel/BaseRepositorio.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+
+namespace Pizzaria.Infra.Repositorios.Excel
+{
+    public class BaseRepositorio
+    {
+        public OleDbConnection OleCon;
+        public OleDbCommand OleCmd;
+        public static String Arquivo = ConfigurationManager.AppSettings["ConexaoPlanilha"];
+        private readonly String StringConexao = string.Format(ConfigurationManager.AppSettings["Conexao"], Arquivo);
+        public String Consulta;
+
+        public void FinishConnection()
+        {
+            if (OleCmd != null)
+            {
+                OleCmd.Parameters.Clear();
+                OleCmd.Dispose();
+            }
+
+            OleCmd = null;
+
+            if (OleCon != null)
+            {
+                if (OleCon.State == ConnectionState.Open)
+                    OleCon.Close();
+
+                OleCon.Dispose();
+            }
+
+            OleCon = null;
+        }
+        public void Conexão()
+        {
+            OleCon = new OleDbConnection(StringConexao);
+            OleCon.Open();
+
+            OleCmd = new OleDbCommand();
+            OleCmd.Connection = OleCon;
+            OleCmd.CommandType = CommandType.Text;
+        }
+    }
+}
---
+++ Alunos/Pizzaria/Pizzaria.WindowsForms/PizzaForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Pizzaria.WindowsForms
+{
+    public partial class PizzaForm : Form
+    {
+        public PizzaForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
