66490ef23a805480cfdc353cd6f633990f2cbaca
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2013-05-31 18:13:47 +0000

    Reestruturação das pastas e projetos.

---
+++ Alunos/Pizzaria/Pizzaria.Infra.Repositorios.Excel/PizzaRepositorio.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data.OleDb;
+using Pizzaria.Dominio;
+
+namespace Pizzaria.Infra.Repositorios.Excel
+{
+    public class PizzaRepositorio : BaseRepositorio
+    {
+        public void Insertpizza(Pizza pizza)
+        {
+            OleCmd.CommandText = "SELECT CodigoPizza FROM [pizza$]";
+            var leitor = OleCmd.ExecuteReader();
+            
+            while (leitor.Read())
+            {
+                pizza.Codigoo = leitor.GetValue(0).ToString();
+            }
+            leitor.Close();
+            
+            var codigo = Convert.ToInt32(pizza.Codigoo);
+            codigo = codigo + 1;
+            pizza.Codigoo = codigo.ToString();
+            
+            Consulta = "INSERT INTO [pizza$] ";
+            Consulta += "([CodigoPizza],[TipoPizza],[PrecoPizza],[CustoPizza]) ";
+            Consulta += "VALUES ";
+            Consulta += "(@Codigopizza,@tipopizza,@precopizza,@custopizza)";
+            OleCmd.CommandText = Consulta;
+            OleCmd.Parameters.Add("@Codigopizza", OleDbType.Integer).Value = Convert.ToInt32(pizza.Codigoo);
+            OleCmd.Parameters.Add("@tipopizza", OleDbType.VarChar, 255).Value = pizza.Tipo;
+            OleCmd.Parameters.Add("@precopizza", OleDbType.VarChar, 255).Value = pizza.Preco;
+            OleCmd.Parameters.Add("@custopizza", OleDbType.VarChar, 255).Value = pizza.Custo;
+
+            OleCmd.ExecuteNonQuery();
+
+            OleCmd.Parameters.Clear();
+        }
+    }
+}
---
+++ Alunos/Pizzaria/Pizzaria.infra.Apoio/Formulario.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Pizzaria.infra.Apoio
+{
+    #region classformulariopizza
+    public class Formulariopizza
+    {
+        /// <summary>
+        /// Método que avalia se os campos obrigatórios estão preenchidos.
+        /// </summary>
+        /// <param name="formulario">Formulário a ser validado</param>
+        /// <param name="provedorDeErros">ErrorProvider do formulário</param>
+        /// <returns>True se todos os campos estiverem preenchidos</returns>
+        public static bool ValidarCamposObrigatorios(Form formulario, ErrorProvider provedorDeErros)
+        {
+            //bool validacao = true;
+            var validacao = true;
+
+            foreach (Control controle in formulario.Controls)
+            {
+                if (controle.Tag != null &&
+                    controle.Tag.ToString().Contains("*"))
+                {
+                    if (controle.Text == string.Empty)
+                    {
+                        provedorDeErros.SetError(controle, "Campo obrigatório.");
+                        controle.Focus();
+                        validacao = false;
+                    }
+                    else
+                    {
+                        provedorDeErros.SetError(controle, string.Empty);
+                    }
+                }
+            }
+
+            return validacao;
+        }
+
+        public static bool ValidarTipoDados(Form formulario, ErrorProvider provedorDeErros)
+        {
+            var validacao = true;
+
+            foreach (Control controle in formulario.Controls)
+            {
+                try
+                {
+                    if (controle.Tag != null &&
+                controle.Tag.ToString().ToUpper().Contains("*DECIMAL"))
+                    {
+                        Convert.ToDecimal(controle.Text);
+                    }
+                    else if (controle.Tag != null &&
+                        controle.Tag.ToString().ToUpper().Contains("APENASTEXTO"))
+                    {
+                        foreach (char caractere in controle.Text)
+                        {
+                            if ((int)caractere >= 48 && (int)caractere <= 57)
+                            {
+                                throw new Exception();
+                            }
+                        }
+                    }
+
+                }
+                catch
+                {
+                    provedorDeErros.SetError(controle, "Valor inválido.");
+                    controle.Focus();
+                    validacao = false;
+                }
+            }
+
+            return validacao;
+        }
+
+        public static void Limpar(Form formulario)
+        {
+            foreach (Control controle in formulario.Controls)
+            {
+                if (controle is TextBox || controle is MaskedTextBox)
+                {
+                    controle.Text = string.Empty;
+                }
+                else if (controle is ComboBox)
+                {
+                    //((ComboBox)controle).SelectedIndex = -1;
+                    (controle as ComboBox).SelectedIndex = -1;
+                }
+            }
+        }
+    }
+    #endregion
+
+    #region calssformulariorefri
+    public class Formulariorefri
+    {
+        /// <summary>
+        /// Método que avalia se os campos obrigatórios estão preenchidos.
+        /// </summary>
+        /// <param name="formulario">Formulário a ser validado</param>
+        /// <param name="provedorDeErros">ErrorProvider do formulário</param>
+        /// <returns>True se todos os campos estiverem preenchidos</returns>
+        public static bool ValidarCamposObrigatorios(Form formulario, ErrorProvider provedorDeErros)
+        {
+            //bool validacao = true;
+            var validacao = true;
+
+            foreach (Control controle in formulario.Controls)
+            {
+                if (controle.Tag != null &&
+                    controle.Tag.ToString().Contains("¨"))
+                {
+                    if (controle.Text == string.Empty)
+                    {
+                        provedorDeErros.SetError(controle, "Campo obrigatório.");
+                        controle.Focus();
+                        validacao = false;
+                    }
+                    else
+                    {
+                        provedorDeErros.SetError(controle, string.Empty);
+                    }
+                }
+            }
+
+            return validacao;
+        }
+
+        public static bool ValidarTipoDados(Form formulario, ErrorProvider provedorDeErros)
+        {
+            var validacao = true;
+
+            foreach (Control controle in formulario.Controls)
+            {
+                try
+                {
+                    if (controle.Tag != null &&
+                controle.Tag.ToString().ToUpper().Contains("¨DECIMAL"))
+                    {
+                        Convert.ToDecimal(controle.Text);
+                    }
+                    else if (controle.Tag != null &&
+                        controle.Tag.ToString().ToUpper().Contains("APENASTEXTO"))
+                    {
+                        foreach (char caractere in controle.Text)
+                        {
+                            if ((int)caractere >= 48 && (int)caractere <= 57)
+                            {
+                                throw new Exception();
+                            }
+                        }
+                    }
+
+                }
+                catch
+                {
+                    provedorDeErros.SetError(controle, "Valor inválido.");
+                    controle.Focus();
+                    validacao = false;
+                }
+            }
+
+            return validacao;
+        }
+
+        public static void Limpar(Form formulario)
+        {
+            foreach (Control controle in formulario.Controls)
+            {
+                if (controle is TextBox)
+                {
+                    controle.Text = string.Empty;
+                }
+                else if (controle is ComboBox)
+                {
+                    //((ComboBox)controle).SelectedIndex = -1;
+                    (controle as ComboBox).SelectedIndex = -1;
+                }
+            }
+        }
+    }
+    #endregion
+
+    #region calssformularioesfiha
+    public class Formularioesfiha
+    {
+        /// <summary>
+        /// Método que avalia se os campos obrigatórios estão preenchidos.
+        /// </summary>
+        /// <param name="formulario">Formulário a ser validado</param>
+        /// <param name="provedorDeErros">ErrorProvider do formulário</param>
+        /// <returns>True se todos os campos estiverem preenchidos</returns>
+        public static bool ValidarCamposObrigatorios(Form formulario, ErrorProvider provedorDeErros)
+        {
+            //bool validacao = true;
+            var validacao = true;
+
+            foreach (Control controle in formulario.Controls)
+            {
+                if (controle.Tag != null &&
+                    controle.Tag.ToString().Contains("°"))
+                {
+                    if (controle.Text == string.Empty)
+                    {
+                        provedorDeErros.SetError(controle, "Campo obrigatório.");
+                        controle.Focus();
+                        validacao = false;
+                    }
+                    else
+                    {
+                        provedorDeErros.SetError(controle, string.Empty);
+                    }
+                }
+            }
+
+            return validacao;
+        }
+
+        public static bool ValidarTipoDados(Form formulario, ErrorProvider provedorDeErros)
+        {
+            var validacao = true;
+
+            foreach (Control controle in formulario.Controls)
+            {
+                try
+                {
+                    if (controle.Tag != null &&
+                controle.Tag.ToString().ToUpper().Contains("°DECIMAL"))
+                    {
+                        Convert.ToDecimal(controle.Text);
+                    }
+                    else if (controle.Tag != null &&
+                        controle.Tag.ToString().ToUpper().Contains("APENASTEXTO"))
+                    {
+                        foreach (char caractere in controle.Text)
+                        {
+                            if ((int)caractere >= 48 && (int)caractere <= 57)
+                            {
+                                throw new Exception();
+                            }
+                        }
+                    }
+
+                }
+                catch
+                {
+                    provedorDeErros.SetError(controle, "Valor inválido.");
+                    controle.Focus();
+                    validacao = false;
+                }
+            }
+
+            return validacao;
+        }
+
+        public static void Limpar(Form formulario)
+        {
+            foreach (Control controle in formulario.Controls)
+            {
+                if (controle is TextBox)
+                {
+                    controle.Text = string.Empty;
+                }
+                else if (controle is ComboBox)
+                {
+                    //((ComboBox)controle).SelectedIndex = -1;
+                    (controle as ComboBox).SelectedIndex = -1;
+                }
+            }
+        }
+    }
+    #endregion
+
+}
