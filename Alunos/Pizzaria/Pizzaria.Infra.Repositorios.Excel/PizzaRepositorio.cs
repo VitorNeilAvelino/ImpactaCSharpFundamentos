900572405d8409f3f06775dfa0a1c336a714a54c
vitoravelino_cp <SND\vitoravelino_cp>
2013-05-31 21:00:36 +0000

    Adição de método salvar para Excel.

--- Alunos/Pizzaria/Pizzaria.Infra.Repositorios.Excel/PizzaRepositorio.cs
+++ Alunos/Pizzaria/Pizzaria.Infra.Repositorios.Excel/PizzaRepositorio.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Data.OleDb;
+using System.IO;
 using Pizzaria.Dominio;
+// Referência adicionada para Microsoft.Office.Interop.Excel, selecione sua versão predileta, abaixo fiz testes com a versão 14.
+using ExcelApp = Microsoft.Office.Interop.Excel;
+
 
 namespace Pizzaria.Infra.Repositorios.Excel
 {
     public class PizzaRepositorio : BaseRepositorio
     {
-        public void Insertpizza(Pizza pizza)
+        public void Insert(Pizza pizza)
         {
             OleCmd.CommandText = "SELECT CodigoPizza FROM [pizza$]";
             var leitor = OleCmd.ExecuteReader();
@@ -34,6 +38,30 @@ namespace Pizzaria.Infra.Repositorios.Excel
             OleCmd.ExecuteNonQuery();
 
             OleCmd.Parameters.Clear();
+        }
+
+        public void Salvar(string caminho, string registro)
+        {
+            var excel = new ExcelApp.Application();
+            var pastaDeTrabalho = File.Exists(caminho) ? excel.Workbooks.Open(caminho) : excel.Workbooks.Add();
+            ExcelApp.Worksheet planilha = pastaDeTrabalho.ActiveSheet;
+
+            var proximaLinha = planilha.Range["A1", "A1"].Value2 == null ? 1 : planilha.UsedRange.Rows.Count + 1;
+            var proximaColuna = 1;
+
+            foreach (var campo in registro.Split(';'))
+            {
+                planilha.Cells[proximaLinha, proximaColuna++] = campo;
+            }
+
+            excel.DisplayAlerts = false;
+
+            pastaDeTrabalho.SaveAs(caminho);
+
+            pastaDeTrabalho.Close();
+            planilha = null;
+            pastaDeTrabalho = null;
+            excel.Quit();
         }
     }
 }
