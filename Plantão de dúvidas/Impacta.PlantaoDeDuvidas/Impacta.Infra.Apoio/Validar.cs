9280a3d2b9b97e2fda7d6ab6cbb6b6ace2db9d59
vitoravelino_cp <SND\vitoravelino_cp>
2013-06-05 17:41:14 +0000

    Teste com validação de XML.

---
+++ Plantão de dúvidas/Impacta.PlantaoDeDuvidas/Impacta.Infra.Apoio/Validar.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace Impacta.Infra.Apoio
+{
+    public class Validar
+    {
+        /// <summary>
+        /// Valida um XML a partir de um XSD.
+        /// </summary>
+        public static void Xml(XDocument xml, string xsd)
+        {
+            var xsdSchema = new XmlSchemaSet();
+            xsdSchema.Add(XmlSchema.Read(new StringReader(xsd), null));
+
+            xml.Validate(xsdSchema, (o, e) =>
+            {
+                if (e.Exception != null)
+                    throw new XmlException(string.Format("Erro na validação do XML de entrada: {0}", e.Message), e.Exception);
+            });
+        }
+    }
+}
---
+++ Plantão de dúvidas/Impacta.PlantaoDeDuvidas/Impacta.Testes/ValidarTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml;
+using Impacta.Infra.Apoio;
+using Impacta.Testes.Properties;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml.Linq;
+
+namespace Impacta.Testes
+{
+    [TestClass]
+    public class ValidarTest
+    {
+        [TestMethod]
+        public void XmlTest()
+        {
+            var xml = XDocument.Parse(Resources.K033Xml);
+            var xsd = Resources.K033Xsd;
+            Validar.Xml(xml, xsd);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(XmlException))]
+        public void XmlMalFormadoTest()
+        {
+            var xml = XDocument.Parse(Resources.K033XmlMalFormado);
+            var xsd = Resources.K033Xsd;
+            Validar.Xml(xml, xsd);
+        }
+    }
+}
