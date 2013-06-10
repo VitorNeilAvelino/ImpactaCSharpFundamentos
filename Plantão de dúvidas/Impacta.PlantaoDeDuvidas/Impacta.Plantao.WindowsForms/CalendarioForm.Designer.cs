34c149e8eb5f0ae7e41729cd56cc9eafa41dce4e
vitoravelino_cp <SND\vitoravelino_cp>
2013-06-10 02:14:13 +0000

    Calendário.

---
+++ Plantão de dúvidas/Impacta.PlantaoDeDuvidas/Impacta.Plantao.WindowsForms/CalendarioForm.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Impacta.Plantao.WindowsForms
+{
+    partial class CalendarioForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calendarioTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.calendarioTableLayoutPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // calendarioTableLayoutPanel
+            // 
+            this.calendarioTableLayoutPanel.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.calendarioTableLayoutPanel.ColumnCount = 7;
+            this.calendarioTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.calendarioTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28572F));
+            this.calendarioTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28572F));
+            this.calendarioTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28572F));
+            this.calendarioTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28572F));
+            this.calendarioTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28572F));
+            this.calendarioTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28572F));
+            this.calendarioTableLayoutPanel.Controls.Add(this.label7, 6, 0);
+            this.calendarioTableLayoutPanel.Controls.Add(this.label6, 5, 0);
+            this.calendarioTableLayoutPanel.Controls.Add(this.label5, 4, 0);
+            this.calendarioTableLayoutPanel.Controls.Add(this.label4, 3, 0);
+            this.calendarioTableLayoutPanel.Controls.Add(this.label3, 2, 0);
+            this.calendarioTableLayoutPanel.Controls.Add(this.label2, 1, 0);
+            this.calendarioTableLayoutPanel.Controls.Add(this.label1, 0, 0);
+            this.calendarioTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.calendarioTableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.calendarioTableLayoutPanel.Name = "calendarioTableLayoutPanel";
+            this.calendarioTableLayoutPanel.RowCount = 7;
+            this.calendarioTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.calendarioTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.calendarioTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.calendarioTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.calendarioTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.calendarioTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.calendarioTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.calendarioTableLayoutPanel.Size = new System.Drawing.Size(949, 570);
+            this.calendarioTableLayoutPanel.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(4, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Domingo";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(139, 1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Segunda";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(274, 1);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Terça";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(409, 1);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Quarta";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(544, 1);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(128, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Quinta";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(679, 1);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(128, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Sexta";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(814, 1);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(131, 25);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Sábado";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CalendarioForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(949, 570);
+            this.Controls.Add(this.calendarioTableLayoutPanel);
+            this.Name = "CalendarioForm";
+            this.Text = "Calendário";
+            this.Load += new System.EventHandler(this.CalendarioForm_Load);
+            this.calendarioTableLayoutPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel calendarioTableLayoutPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Plantão de dúvidas/Impacta.PlantaoDeDuvidas/Impacta.Plantao.WindowsForms/CalendarioForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Impacta.Plantao.WindowsForms
+{
+    public partial class CalendarioForm : Form
+    {
+        public CalendarioForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CalendarioForm_Load(object sender, EventArgs e)
+        {
+            PopularCalendario(DateTime.Now.Month, DateTime.Now.Year);
+        }
+
+        private void PopularCalendario(int numeroMes, int ano)
+        {
+            //var diasDoMes = DateTime.DaysInMonth(ano, numeroMes);
+
+            var dia = new DateTime(ano, numeroMes, 1);
+            var label = new Label { Text = "1", Font = new Font("Microsoft Sans Serif", 11F, FontStyle.Regular, GraphicsUnit.Point, 0) };
+
+            calendarioTableLayoutPanel.Controls.Add(label, (int)dia.DayOfWeek, 1);
+
+            //for (var i = 0; i <= calendarioTableLayoutPanel.RowCount - 1; i++)
+            //{
+            //    for (var j = 0; j <= calendarioTableLayoutPanel.ColumnCount - 1; j++)
+            //    {
+            //        for (var k = 0; k < DateTime.DaysInMonth(ano, numeroMes); k++)
+            //        {
+            //            var dia = new DateTime(ano, numeroMes, k);
+
+            //            if (dia.DayOfWeek == calendarioTableLayoutPanel.row)
+            //            {
+
+            //            }
+            //        }
+            //    }
+            //}
+        }
+    }
+}
