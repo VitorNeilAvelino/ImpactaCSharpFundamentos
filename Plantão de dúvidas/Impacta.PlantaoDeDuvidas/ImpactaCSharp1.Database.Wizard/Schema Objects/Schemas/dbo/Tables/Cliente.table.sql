f9aecd29103251bace6d60a57c6112c38d548738
vitoravelino_cp <SND\vitoravelino_cp>
2013-06-11 02:15:46 +0000

    Wizard para database project.

---
+++ Plantão de dúvidas/Impacta.PlantaoDeDuvidas/ImpactaCSharp1.Database.Wizard/Schema Objects/Schemas/dbo/Tables/Cliente.table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Cliente] (
+    [Id]             INT             IDENTITY (1, 1) NOT NULL,
+    [Pessoa_Id]      INT             NOT NULL,
+    [Renda]          DECIMAL (18, 2) NOT NULL,
+    [DataNascimento] DATETIME        NOT NULL
+);
+
---
+++ Plantão de dúvidas/Impacta.PlantaoDeDuvidas/ImpactaCSharp1.Database.Wizard/Schema Objects/Schemas/dbo/Tables/Keys/FK_PessoaEnderecos_Cidade.fkey.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE [dbo].[PessoaEnderecos]
+    ADD CONSTRAINT [FK_PessoaEnderecos_Cidade] FOREIGN KEY ([Cidade_Id]) REFERENCES [dbo].[Cidade] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
