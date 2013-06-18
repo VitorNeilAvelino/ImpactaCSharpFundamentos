d7e408e86ade5e732414e99d35d83feda0c40549
vitoravelino_cp <SND\vitoravelino_cp>
2013-06-18 02:25:43 +0000

    Execução do script de criação do banco junto com o setup do aplicativo.

---
+++ Plantão de dúvidas/Impacta.PlantaoDeDuvidas/Impacta.Testes/SqlScriptDeployTest.cs
@@ -0,0 +1,18 @@
+﻿using Impacta.Infra.Apoio.Setup;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+
+namespace Impacta.Testes
+{
+    [TestClass]
+    public class SqlScriptDeployTest
+    {
+        [TestMethod]
+        public void InstallTest()
+        {
+            var target = new SqlScriptDeploy();
+            IDictionary statesaver = new Hashtable();
+            target.Install(statesaver);
+        }
+    }
+}
