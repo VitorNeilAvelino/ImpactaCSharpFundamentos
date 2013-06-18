0ec09df480f9f53dc7ba53daf04f75cc70fe36a7
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2013-06-18 02:25:43 +0000

    Execução do script de criação do banco junto com o setup do aplicativo.

--- Plantão de dúvidas/Impacta.PlantaoDeDuvidas/Impacta.Infra.Apoio.Setup/SqlScriptDeploy.cs
+++ Plantão de dúvidas/Impacta.PlantaoDeDuvidas/Impacta.Infra.Apoio.Setup/SqlScriptDeploy.cs
@@ -1,58 +1,56 @@
-﻿using System.ComponentModel;
-using System.Configuration;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Reflection;
-using System.Data.SqlClient;
-using Impacta.Infra.Apoio.Setup.Properties;
 
 namespace Impacta.Infra.Apoio.Setup
 {
     [RunInstaller(true)]
     public partial class SqlScriptDeploy : System.Configuration.Install.Installer
     {
-        readonly SqlConnection _sqlConnection = new SqlConnection();
-
         public SqlScriptDeploy()
         {
             InitializeComponent();
         }
 
-        private static string GetSql()
+        private IEnumerable<FileInfo> ObterScriptsSql()
         {
-            var asm = Assembly.GetExecutingAssembly();
-            var stm = asm.GetManifestResourceStream(asm.GetName().Name + "." + "ScriptCriacaoBanco.sql");
-            var reader = new StreamReader(stm);
-            return reader.ReadToEnd();
+            return new DirectoryInfo(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)).GetFiles("*.sql").ToList();
         }
 
-        private void DeployDatabase()
+        private void ExecutarScriptsSql()
         {
-            var sql = GetSql();
-            var command = new SqlCommand(sql, _sqlConnection);
-            //_sqlConnection.ConnectionString = ConfigurationManager.ConnectionStrings["masterConnectionString"].ConnectionString;
-            _sqlConnection.ConnectionString = Settings.Default.masterConnectionString;
-            command.Connection.Open();
-            //command.Connection.ChangeDatabase(dbName);
-
-            try
+            foreach (var script in ObterScriptsSql())
             {
-                command.ExecuteNonQuery();
-            }
-            finally
-            {
-                command.Connection.Close();
+                IniciarProcessoSqlCmd(script);
             }
         }
 
-        //protected void DeployDatabase(string dbName)
-        //{
-        //    ExecuteSql(GetSql("sql.txt"));
-        //}
+        private static void IniciarProcessoSqlCmd(FileInfo script)
+        {
+            using (var sqlCmd = new Process())
+            {
+                //sqlCmd.StartInfo.FileName = @"C:\Program Files\Microsoft SQL Server\110\Tools\Binn\SqlCmd.exe";
+                sqlCmd.StartInfo.FileName = @"SqlCmd.exe";
+                sqlCmd.StartInfo.Arguments = String.Format(@"-S .\sqlexpress -i ""{0}""", script.FullName);
+                sqlCmd.StartInfo.UseShellExecute = false;
+                sqlCmd.StartInfo.CreateNoWindow = true;
+
+                sqlCmd.Start();
+
+                sqlCmd.WaitForExit();
+
+                sqlCmd.Close();
+            }
+        }
 
         public override void Install(System.Collections.IDictionary statesaver)
         {
             base.Install(statesaver);
-            DeployDatabase();
+            ExecutarScriptsSql();
         }
     }
 }
