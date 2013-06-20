5299be4e9184e21e09fdf269f8aa8c84d8f35403
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2013-06-20 18:37:15 +0000

    Excel salvo em abas.

---
+++ Roteiro/ImpactaCSharp1/Impacta.Infra.Repositorios.MsOffice.Testes/ExcelRepositorioTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Impacta.Infra.Repositorios.MsOffice.Testes
+{
+    [TestClass]
+    public class ExcelRepositorioTest
+    {
+        [TestMethod]
+        public void SalvarTest()
+        {
+            const string caminho = @"C:\Users\vavelino\Documents\Excel.xls";
+            const string registro = "Vítor;Avelino";
+            ExcelRepositorio.Salvar(caminho, new List<string> { registro, registro, registro, registro, registro, registro, registro, registro, registro, 
+                registro, registro }, 10);
+        }
+    }
+}
