929e51ae4d0c5643b2c7ef0fdb015529c8d7076a
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2013-08-19 13:23:35 +0000

    Teste com TimeSpan.

--- Plantão de dúvidas/Impacta.PlantaoDeDuvidas/Impacta.Plantao.WebApplication/Default.aspx.designer.cs
+++ Plantão de dúvidas/Impacta.PlantaoDeDuvidas/Impacta.Plantao.WebApplication/Default.aspx.designer.cs
@@ -11,14 +11,5 @@ namespace Impacta.Plantao.WebApplication {
     
     
     public partial class _Default {
-        
-        /// <summary>
-        /// ReportViewer1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::Microsoft.Reporting.WebForms.ReportViewer ReportViewer1;
     }
 }
---
+++ Plantão de dúvidas/Impacta.PlantaoDeDuvidas/Impacta.Testes/IntervaloDatasTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Impacta.Testes
+{
+    [TestClass]
+    public class IntervaloDatasTest
+    {
+        [TestMethod]
+        public void TestarIntervaloDatas()
+        {
+            var dataInicial = DateTime.Now;
+            var dataFinal = DateTime.Now.AddDays(10);
+
+            var intervaloDatas = dataFinal - dataInicial;
+
+            Assert.AreEqual(intervaloDatas.Days, 10);
+        }
+    }
+}
