e749f6f432e2f3f923045255add88eea01eb67eb
vitoravelino_cp <SND\vitoravelino_cp>
2013-10-13 17:30:01 +0000

    Conversão de int para enum.

---
+++ Roteiro/ImpactaCSharp1/Impacta.Dominio.Teste/SituacaoFinanceiraTest.cs
@@ -0,0 +1,20 @@
+﻿using Impacta.Dominio;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Impacta.Dominio.Teste
+{
+    [TestClass()]
+    public class SituacaoFinanceiraTest
+    {
+        [TestMethod()]
+        public void ConversaoDeEnumTeste()
+        {
+            var situacaoRecuperadaDoBanco = 0;
+            var situacaoConvertidaParaEnum = (SituacaoFinanceira)situacaoRecuperadaDoBanco;
+
+            Assert.AreEqual(situacaoConvertidaParaEnum, SituacaoFinanceira.Aberto);
+            Assert.AreEqual(situacaoConvertidaParaEnum.ToString(), "Aberto");
+        }
+    }
+}
---
+++ Roteiro/ImpactaCSharp1/Impacta.Dominio/SituacaoFinanceira.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Impacta.Dominio
+{
+    public enum SituacaoFinanceira
+    {
+        Aberto = 0,
+        Quitado = 1,
+        Vencido = 2,
+        Cancelado = 3
+    }
+}
