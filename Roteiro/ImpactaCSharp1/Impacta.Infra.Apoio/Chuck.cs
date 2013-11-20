a78391c4f023efee6ae5a5316a4d9a98e8e279fa
vitoravelino_cp <SND\vitoravelino_cp>
2013-11-20 19:23:23 +0000

    KickSort.

---
+++ Roteiro/ImpactaCSharp1/Impacta.Infra.Apoio/Chuck.cs
@@ -0,0 +1,54 @@
+﻿namespace Impacta.Infra.Apoio
+{
+    public static class Chuck
+    {
+        public static void KickSort(int[] vetor)
+        {
+            KickSort(vetor, 0, vetor.Length - 1);
+        }
+
+        private static void KickSort(int[] vetor, int inicio, int fim)
+        {
+            if (inicio < fim)
+            {
+                var posicaoPivo = Particionar(vetor, inicio, fim);
+                KickSort(vetor, inicio, posicaoPivo - 1);
+                KickSort(vetor, posicaoPivo + 1, fim);
+            }
+        }
+
+        private static int Particionar(int[] vetor, int inicio, int fim)
+        {
+            var pivo = vetor[inicio];
+            int i = inicio + 1, f = fim;
+
+            while (i <= f)
+            {
+                if (vetor[i] <= pivo)
+                {
+                    i++;
+                }
+                else if (pivo < vetor[f])
+                {
+                    f--;
+                }
+                else
+                {
+                    Trocar(vetor, i, f);
+                    i++;
+                    f--;
+                }
+            }
+            
+            Trocar(vetor, inicio, f);
+            return f;
+        }
+
+        private static void Trocar(int[] vetor, int i, int f)
+        {
+            var aux = vetor[i];
+            vetor[i] = vetor[f];
+            vetor[f] = aux;
+        }
+    }
+}
