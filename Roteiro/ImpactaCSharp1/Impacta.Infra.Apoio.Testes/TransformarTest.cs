0b9203b150a14fe585f341197a935de2a5741f73
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2013-11-20 16:19:38 +0000

    Quicksort.

---
+++ Roteiro/ImpactaCSharp1/Impacta.Infra.Apoio.Testes/TransformarTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Impacta.Infra.Apoio.Testes
+{
+    [TestClass]
+    public class TransformarTest
+    {
+        [TestMethod]
+        public void OrdenarTest()
+        {
+            int[] vetor = { 1, 2, 0, 9, 20, 11 };
+            Transformar.Ordenar(vetor);
+
+            foreach (var elemento in vetor)
+            {
+                Console.Write("{0} ", elemento);
+            }
+        }
+    }
+}
--- Roteiro/ImpactaCSharp1/Impacta.Infra.Apoio/Transformar.cs
+++ Roteiro/ImpactaCSharp1/Impacta.Infra.Apoio/Transformar.cs
@@ -14,5 +14,50 @@
 
             return texto;
         }
+
+        public static void Ordenar(int[] vetor)
+        {
+            Ordenar(vetor, 0, vetor.Length - 1);
+        }
+
+        /// <summary>
+        /// http://pt.wikipedia.org/wiki/Quicksort#C.23
+        /// </summary>
+        /// <param name="vetor"></param>
+        /// <param name="inicio"></param>
+        /// <param name="fim"></param>
+        private static void Ordenar(int[] vetor, int inicio, int fim)
+        {
+            if (inicio < fim)
+            {
+                var posicaoPivo = Particionar(vetor, inicio, fim);
+                Ordenar(vetor, inicio, posicaoPivo - 1);
+                Ordenar(vetor, posicaoPivo + 1, fim);
+            }
+        }
+
+        private static int Particionar(int[] vetor, int inicio, int fim)
+        {
+            var pivo = vetor[inicio];
+            int i = inicio + 1, f = fim;
+            while (i <= f)
+            {
+                if (vetor[i] <= pivo)
+                    i++;
+                else if (pivo < vetor[f])
+                    f--;
+                else
+                {
+                    int troca = vetor[i];
+                    vetor[i] = vetor[f];
+                    vetor[f] = troca;
+                    i++;
+                    f--;
+                }
+            }
+            vetor[inicio] = vetor[f];
+            vetor[f] = pivo;
+            return f;
+        }
     }
 }
