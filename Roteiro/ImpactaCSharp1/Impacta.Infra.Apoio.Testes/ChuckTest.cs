91b8448683ab07b5b8874849b46045029c15e6b2
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2013-11-20 19:23:23 +0000

    KickSort.

---
+++ Roteiro/ImpactaCSharp1/Impacta.Infra.Apoio.Testes/ChuckTest.cs
@@ -0,0 +1,22 @@
+﻿using Impacta.Infra.Apoio;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Impacta.Infra.Apoio.Testes
+{
+    [TestClass]
+    public class ChuckTest
+    {
+        [TestMethod]
+        public void KickSortTest()
+        {
+            int[] vetor = { 1, 7, 1, 5, 20, 11 };
+            Chuck.KickSort(vetor);
+
+            foreach (var elemento in vetor)
+            {
+                Console.Write("{0} ", elemento);
+            }
+        }
+    }
+}
