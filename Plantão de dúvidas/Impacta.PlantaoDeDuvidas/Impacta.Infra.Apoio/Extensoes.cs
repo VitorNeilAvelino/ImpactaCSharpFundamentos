84ea53f5dc3ab0398b6cec039f933c28b5859771
vitoravelino_cp <SND\vitoravelino_cp>
2013-12-04 01:26:32 +0000

    Palavra triângulo.

---
+++ Plantão de dúvidas/Impacta.PlantaoDeDuvidas/Impacta.Infra.Apoio/Extensoes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Impacta.Infra.Apoio
+{
+    public static class Extensoes
+    {
+        public static T[] RemoveAt<T>(this T[] source, int idx)
+        {
+            var destination = new T[source.Length - 1];
+            if (idx > 0)
+                Array.Copy(source, 0, destination, 0, idx);
+
+            if (idx < source.Length - 1)
+                Array.Copy(source, idx + 1, destination, idx, source.Length - idx - 1);
+
+            return destination;
+        }
+    }
+}
