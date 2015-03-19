f69dc18fe725043d657c590ac0537ea49e8e20b5
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2015-03-19 02:35:06 +0000

    Aula 11.

---
+++ Roteiro/ImpactaCSharp1/Impacta.Dominio/Combustivel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+namespace Impacta.Dominio
+{
+    public enum Combustivel
+    {
+        Gasolina = 1,
+
+        [Description("Álcool")]
+        Alcool = 2,
+
+        Flex = 3,
+
+        [Description("Híbrido")]
+        Hibrido = 4
+    }
+}
