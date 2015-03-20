d5522232008c4a3251eb2a53476cd061a85e8dcb
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2015-03-20 01:09:53 +0000

    Aula 11.

---
+++ Roteiro/ImpactaCSharp1/Impacta.Infra.Repositorios.SistemaDeArquivos.Testes/ModeloRepositorioTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Impacta.Infra.Repositorios.SistemaDeArquivos.Tests
+{
+    [TestClass()]
+    public class ModeloRepositorioTests
+    {
+        [TestMethod()]
+        public void SelecionarTest()
+        {
+            var modeloRepositorio = new ModeloRepositorio();
+
+            var modelos = modeloRepositorio.Selecionar(1);
+
+            foreach (var modelo in modelos)
+            {
+                Console.WriteLine("{0}-{1}-{2}", modelo.Id, modelo.Descricao, modelo.Marca.Nome);
+            }
+        }
+    }
+}
---
+++ Roteiro/ImpactaCSharp1/Impacta.Infra.Repositorios.SistemaDeArquivos/ModeloRepositorio.cs
@@ -0,0 +1,49 @@
+﻿using Impacta.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Xml.Linq;
+
+namespace Impacta.Infra.Repositorios.SistemaDeArquivos
+{
+    public class ModeloRepositorio : IModeloRepositorio
+    {
+        public List<Modelo> Selecionar(int marcaId)
+        {
+            var modelos = new List<Modelo>();
+            var arquivoXml = XDocument.Load(ConfigurationManager.AppSettings["caminhoArquivoModelo"]);
+
+            foreach (var noh in arquivoXml.Descendants("modelo"))
+            {
+                if (noh.Element("marcaId").Value != marcaId.ToString())
+                {
+                    continue;
+                }
+
+                var modelo = new Modelo();
+                modelo.Id = Convert.ToInt32(noh.Element("id").Value);
+                modelo.Descricao = noh.Element("descricao").Value;
+                modelo.Marca = new MarcaRepositorio().Selecionar(marcaId)[0];
+
+                modelos.Add(modelo);
+            }
+
+            return modelos;
+        }
+
+        public void Inserir(Modelo modelo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(Modelo modelo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int modeloId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
