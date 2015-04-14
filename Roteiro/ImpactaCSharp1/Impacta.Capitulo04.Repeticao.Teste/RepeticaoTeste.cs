614e97a32ed84d2dcd4790316665a815b9ce3a1c
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2015-04-14 04:42:48 +0000

    Pedido e corpo do pedido.

--- Roteiro/ImpactaCSharp1/Impacta.Capitulo04.Repeticao.Teste/RepeticaoTeste.cs
+++ Roteiro/ImpactaCSharp1/Impacta.Capitulo04.Repeticao.Teste/RepeticaoTeste.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Impacta.Capitulo04.Repeticao.Teste
 {
@@ -84,6 +87,64 @@ namespace Impacta.Capitulo04.Repeticao.Teste
                     //Directory.Delete(directory, false);
                     Directory.Delete(diretorio);
                 }
+            }
+        }
+
+        [TestMethod]
+        public void LerTodasLinhas()
+        {
+            var linhas = Properties.Resources.testetxt
+                .Split(new[] {Environment.NewLine}, StringSplitOptions.None).ToList();
+
+            const string separador = "============================== Pedido ==============================";
+
+            var pilha = new Stack<string>();
+            var dicionario = new Dictionary<string, StringBuilder>();
+            var linhaEmails = string.Empty;
+            var corpoPedido = new StringBuilder();
+
+            foreach (var linha in linhas)
+            {
+                pilha.Push(linha);
+
+                if (linha == separador)
+                {
+                    if (corpoPedido.Length != 0)
+                    {
+                        pilha.Pop();
+                        corpoPedido.Replace(pilha.Peek(), string.Empty);
+                        
+                        dicionario.Add(linhaEmails, corpoPedido);
+
+                        linhaEmails = pilha.Pop();
+                        corpoPedido = new StringBuilder();
+                    }
+                }
+                else
+                {
+                    if (linhaEmails == string.Empty)
+                    {
+                        linhaEmails = pilha.Pop();
+                    }
+                    else
+                    {
+                        corpoPedido.AppendLine(linha); 
+                    }
+                }
+            }
+
+            dicionario.Add(linhaEmails, corpoPedido);
+            
+            foreach (var item in dicionario)
+            {
+                Console.WriteLine(new string('-', 200));
+                Console.WriteLine("Email {0}", item.Key);
+                Console.WriteLine(new string('-', 200));
+                Console.WriteLine();
+                Console.WriteLine("Pedido".PadRight(200, '-'));
+
+                Console.WriteLine(item.Value.ToString());
+                Console.WriteLine("Fim pedido".PadRight(300, '|'));
             }
         }
     }
