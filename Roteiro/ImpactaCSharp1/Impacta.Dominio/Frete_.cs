6d734c7b7aebc464e3d05d266b23463c7994fedb
vitoravelino_cp <SND\vitoravelino_cp>
2015-04-15 13:02:33 +0000

    Frete_.

---
+++ Roteiro/ImpactaCSharp1/Impacta.Dominio/Frete_.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Impacta.Dominio
+{
+    public class Frete_
+    {
+        public Frete_(string uf, decimal valor)
+        {
+            Uf = uf;
+            Valor = valor;
+
+            Calcular();
+        }
+
+        private void Calcular()
+        {
+            if (Uf.ToUpper() == "SP")
+            {
+                Percentual = 0.2m;
+            }
+            else
+            {
+                Percentual = 0.3m;
+            }
+        }
+        
+        public string Uf { get; set; }
+        public decimal Valor { get; set; }
+
+        public decimal Percentual { get; set; }
+        public decimal Total { get { return Valor * Percentual; } }
+    }
+}
