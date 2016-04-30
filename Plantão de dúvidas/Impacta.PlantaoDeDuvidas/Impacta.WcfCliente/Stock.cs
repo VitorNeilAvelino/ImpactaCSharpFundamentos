13447398cb88671718759076a906e6543263d03e
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2016-04-30 00:24:52 +0000

    Call WCF service from SQL Server procedure.

---
+++ Plantão de dúvidas/Impacta.PlantaoDeDuvidas/Impacta.WcfCliente/Stock.cs
@@ -0,0 +1,15 @@
+﻿using Impacta.WcfClient.StockServiceReference;
+
+namespace Impacta.WcfClient
+{
+    public class Stock
+    {
+        public static string Get(string quote)
+        {
+            using (var stockClient = new StockQuoteSoapClient())
+            {
+                return stockClient.GetQuote(quote);
+            }
+        }
+    }
+}
