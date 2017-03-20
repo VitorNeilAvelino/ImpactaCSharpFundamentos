7f3c524f25362a1ff2a4478ee5b92b05724bb9a0
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2017-03-20 17:20:12 +0000

    ContarErros em ErrorProvider.

---
+++ Roteiro/ImpactaCSharp1/Impacta.Infra.Apoio.Testes/FormularioTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Forms;
+
+namespace Impacta.Apoio.Tests
+{
+    [TestClass()]
+    public class FormularioTests
+    {
+        static ErrorProvider provedorDeErro = new ErrorProvider();
+        static Form formulario = new Form();
+
+        [ClassInitialize]
+        public static void Inicializar(TestContext contexto)
+        {
+            provedorDeErro.ContainerControl = formulario;
+        }
+
+        [TestMethod()]
+        public void ValidacaoObrigatorioTest()
+        {
+            var nomeTextBox = new TextBox();
+            nomeTextBox.Tag = "*";
+            nomeTextBox.Text = "Texto";
+
+            formulario.Controls.Add(nomeTextBox);
+
+            var validacao = Formulario.Validar(formulario, provedorDeErro);
+
+            Assert.IsTrue(validacao);          
+            
+            nomeTextBox.Text = "";
+
+            validacao = Formulario.Validar(formulario, provedorDeErro);
+
+            Assert.IsFalse(validacao);
+
+            formulario.Controls.Remove(nomeTextBox);
+        }
+
+        [TestMethod]
+        public void ValidacaoTipoDadoTeste()
+        {
+            var placaTextBox = new TextBox();
+            placaTextBox.Tag = "PLACA";
+            placaTextBox.Text = "ABC1234";
+
+            formulario.Controls.Add(placaTextBox);
+
+            var validacao = Formulario.Validar(formulario, provedorDeErro);
+
+            Assert.IsTrue(validacao);
+        }
+    }
+}
